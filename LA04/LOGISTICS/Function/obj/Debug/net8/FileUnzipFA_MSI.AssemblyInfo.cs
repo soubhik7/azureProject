f4da69acf41ed3ca31beda724585ffd818a13e12
soubhik <sou7bhik@gmail.com>
2024-07-20 09:37:46 +0530

    update

--- LA04/LOGISTICS/Function/obj/Debug/net8/FileUnzipFA_MSI.AssemblyInfo.cs
+++ LA04/LOGISTICS/Function/obj/Debug/net8/FileUnzipFA_MSI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FileUnzipFA_MSI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a55c1afd93a1a23057a482f6a8779a50ca002a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+938400737b16f4148d48976081d932be45eef9e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("FileUnzipFA_MSI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FileUnzipFA_MSI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
